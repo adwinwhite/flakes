876841fa507908a78bc1ecdf6822573443b65993
Adwin White <adwinw01@gmail.com>
2021-12-14 20:05:23 +0800

    added neovim config

--- flake.nix
+++ flake.nix
@@ -1,26 +1,42 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     neovim = {
       url = "github:nix-community/neovim-nightly-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
 
     nixpkgs-wayland = {
       url = "github:colemickens/nixpkgs-wayland";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    v2t = {
+      url = "github:adwingray/v2ray-tools";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
     };
     
   };
   outputs = inputs@{ self, nixpkgs, ... }: {
     nix.registry.nixpkgs.flake = nixpkgs;
-    nixosConfigurations.tardis = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.bluespace = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ 
         ./configuration.nix
@@ -29,6 +45,8 @@
           nixpkgs.overlays = [
             inputs.neovim.overlay
             inputs.nixpkgs-wayland.overlay
+            inputs.v2t.overlay
+            inputs.rust-overlay.overlay
           ];
           nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
           nix.registry.p.flake = self;
