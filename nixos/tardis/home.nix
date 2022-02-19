26e073247706c32187fb87fc4abe50ed1f9940bc
Adwin White <adwinw01@gmail.com>
2022-02-19 23:03:21 +0800

    added light and helix

--- nixos/tardis/home.nix
+++ nixos/tardis/home.nix
@@ -1,6 +1,8 @@
 { pkgs, lib, config, ...}:
 {
   home.packages = with pkgs; [
+    light
+    helix
     drill
     socat
     htop
@@ -44,6 +46,7 @@
     rage
     sops
     tdesktop
+    qv2ray
   ];
 
   xdg = {
@@ -170,6 +173,26 @@
     };
     alacritty = {
       enable = true;
+      settings = {
+        font = {
+          normal = {
+            family = "Source Code Pro for Powerline";
+            style = "Regular";
+          };
+          bold = {
+            family = "Source Code Pro for Powerline";
+            style = "Bold";
+          };
+          italic = {
+            family = "Source Code Pro for Powerline";
+            style = "Italic";
+          };
+          bold_italic = {
+            family = "Source Code Pro for Powerline";
+            style = "Bold Italic";
+          };
+        };
+      };
     };
     neovim = import ./neovim.nix { 
       inherit pkgs;
