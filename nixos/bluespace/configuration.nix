9e837cf826f178ccaf69badfdf68e3662b2db0a5
Adwin White <adwinw01@gmail.com>
2022-04-06 19:04:36 +0800

    fix wireguard-tool update for bluespace

--- nixos/bluespace/configuration.nix
+++ nixos/bluespace/configuration.nix
@@ -209,7 +209,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    wireguard
     wireguard-tools
     frp
     v2ray
