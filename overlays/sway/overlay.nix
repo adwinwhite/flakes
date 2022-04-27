42c9bd5cb56a6e3c13231032fc716a90e8d14ee3
Adwin White <adwinw01@gmail.com>
2022-04-27 19:56:50 +0800

    add inhibit_fullscreen for sway

--- overlays/sway/overlay.nix
+++ overlays/sway/overlay.nix
@@ -2,6 +2,7 @@ final: prev: {
   sway-unwrapped = prev.sway-unwrapped.overrideAttrs (old: {
     patches = (old.patches or []) ++ [
       ./ime_popup.patch
+      ./inhibit_fullscreen.patch
     ];
   });
 }
