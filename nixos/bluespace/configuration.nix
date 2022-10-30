93d7a541214269f69e751eed0ac45a0de80bbfe6
Adwin White <adwinw01@gmail.com>
2022-10-30 13:10:13 +0800

    update: new bluspace config for new vps

--- nixos/bluespace/configuration.nix
+++ nixos/bluespace/configuration.nix
@@ -10,12 +10,11 @@
       ./hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = [ 
-    (pkgs.callPackage ../../pkgs/rtl8188gu { kernel = pkgs.linux; })
-    (pkgs.callPackage ../../pkgs/rtl8821cu { kernel = pkgs.linux; })
+  boot.cleanTmpDir = true;
+  zramSwap.enable = true;
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCXwWJH+vsxzntGdP0Cn4piSp1Tocg98YIrvzQIaHbL9wW/1Jj5w9bOYtFP5XbWQSQjbHqH04ISB6boV08Pb41Fs3iCwVXMdUa6qkRh9z0UmXq74Vp58AJ3ONQFOQM/IYkbMFVWE1TjbrXlA/dpPhXKBdCj2ZA7gParqXEfk6KAVNKnFED02YvqoVotOzcfH9nlsMzMRVpfm6he0aP04RZE/Bs/UXzuQXZEwnOBYpuDSLW+CQcoxGhEKgTxgDnfdLNqYyp6rVHWy0+b46fbx1JVU02xMH8YplrIC/b/ysBVboCPf79gZPnw7jQNf+EX9sAm2bNuje1DSSGqivpLe199 adwin@Tardis" 
   ];
 
   # Enable flakes and gc
@@ -36,16 +35,12 @@
   nix = {
     settings = {
       substituters = pkgs.lib.mkBefore [
-        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" 
-        "https://mirrors.ustc.edu.cn/nix-channels/store" 
         "https://nix-community.cachix.org"
-        "https://nixpkgs-wayland.cachix.org"
-        "https://berberman.cachix.org"
       ];
       builders-use-substitutes = true;
       auto-optimise-store = true;
-      trusted-users = [ "root" "adwin" "liugc" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" "berberman.cachix.org-1:UHGhodNXVruGzWrwJ12B1grPK/6Qnrx2c3TjKueQPds=" ];
+      trusted-users = [ "root" "adwin" ];
+      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
   };
 
@@ -61,144 +56,85 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  networking = rec {
+  networking = {
     hostName = "bluespace";
     # to use fail2ban I have to enable firewall though I dont' really need it
     firewall = {
       enable = true;
-      allowedTCPPortRanges = [
-        { from = 1; to = 65535; }
-      ];
-      allowedUDPPortRanges = [
-        { from = 1; to = 65535; }
-      ];
+      allowedTCPPorts = [ 22 ];
     };
 
-    useDHCP = false;
-    interfaces.enp0s3.useDHCP = true;
-    networkmanager = {
-      enable = true;
-      dns = "dnsmasq";
-    };
-    proxy.default = "http://127.0.0.1:10809";
-    proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # useDHCP = false;
+    # interfaces.enp0s3.useDHCP = true;
 
-    nat = {
-      enable = true;
-      externalInterface = "wlp0s20u11";
-      internalInterfaces = [ "wg0" ];
-    };
+    # nat = {
+      # enable = true;
+      # externalInterface = "wlp0s20u11";
+      # internalInterfaces = [ "wg0" ];
+    # };
 
-    wireguard.interfaces = {
-      # "wg0" is the network interface name. You can name the interface arbitrarily.
-      wg0 = {
-        # Determines the IP address and subnet of the server's end of the tunnel interface.
-        ips = [ "10.100.0.3/24" ];
+    # wireguard.interfaces = {
+      # # "wg0" is the network interface name. You can name the interface arbitrarily.
+      # wg0 = {
+        # # Determines the IP address and subnet of the server's end of the tunnel interface.
+        # ips = [ "10.100.0.3/24" ];
 
-        # The port that WireGuard listens to. Must be accessible by the client.
-        listenPort = 11454;
+        # # The port that WireGuard listens to. Must be accessible by the client.
+        # listenPort = 11454;
 
-        # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
-        # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
-        postSetup = ''
-          ${pkgs.iptables}/bin/iptables -A FORWARD -o ${nat.externalInterface} -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
-        '';
+        # # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+        # # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+        # postSetup = ''
+          # ${pkgs.iptables}/bin/iptables -A FORWARD -o ${nat.externalInterface} -j ACCEPT
+          # ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -o wg0 -j MASQUERADE
+        # '';
 
-        # This undoes the above command
-        postShutdown = ''
-          ${pkgs.iptables}/bin/iptables -D FORWARD -o ${nat.externalInterface} -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
-        '';
+        # # This undoes the above command
+        # postShutdown = ''
+          # ${pkgs.iptables}/bin/iptables -D FORWARD -o ${nat.externalInterface} -j ACCEPT
+          # ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -o wg0 -j MASQUERADE
+        # '';
 
-        # Path to the private key file.
-        #
-        # Note: The private key can also be included inline via the privateKey option,
-        # but this makes the private key world-readable; thus, using privateKeyFile is
-        # recommended.
-        privateKeyFile = "/home/adwin/.wireguard/prikey";
+        # # Path to the private key file.
+        # #
+        # # Note: The private key can also be included inline via the privateKey option,
+        # # but this makes the private key world-readable; thus, using privateKeyFile is
+        # # recommended.
+        # privateKeyFile = "/home/adwin/.wireguard/prikey";
 
-        peers = [
-          # List of allowed peers.
-          { # Feel free to give a meaning full name
-            # Public key of the peer (not a file path).
-            publicKey = "6uNLTaYV8Y3H5O9ZZsxH6Xxf+6KzG6n8NYN538df1zI=";
-            # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-            allowedIPs = [ "10.100.0.0/24" ];
-            endpoint = "175.24.187.39:11454";
-            persistentKeepalive = 15;
-          }
-          {
-            publicKey = "9ihTi1vqN0ei8FSYw88AcuxyV+JraiUE7/Wf/XLiuDI=";
-            allowedIPs = [ "10.100.0.2/32" ];
-          }
-        ];
-      };
-    };
+        # peers = [
+          # # List of allowed peers.
+          # { # Feel free to give a meaning full name
+            # # Public key of the peer (not a file path).
+            # publicKey = "6uNLTaYV8Y3H5O9ZZsxH6Xxf+6KzG6n8NYN538df1zI=";
+            # # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+            # allowedIPs = [ "10.100.0.0/24" ];
+            # endpoint = "175.24.187.39:11454";
+            # persistentKeepalive = 15;
+          # }
+          # {
+            # publicKey = "9ihTi1vqN0ei8FSYw88AcuxyV+JraiUE7/Wf/XLiuDI=";
+            # allowedIPs = [ "10.100.0.2/32" ];
+          # }
+        # ];
+      # };
+    # };
   };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # Enable the X11 windowing system.
-  services = {
-    xserver = {
-      enable = true;
-      layout = "us";
-      displayManager = {
-        defaultSession = "sway";
-        sddm.enable = true;
-      };
-    };
-  };
-  xdg = {
-    portal.wlr.enable = true;
-  };
-
-
-  # Enable the Plasma 5 Desktop Environment.
-  # services.xserver.displayManager.sddm.enable = true;
-  # services.xserver.displayManager.sddm.defaultSession = "sway";
-  # services.xserver.desktopManager.plasma5.enable = true;
-  
-
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.adwin = {
     isNormalUser = true;
-    initialHashedPassword = "emmm";
+    initialHashedPassword = "$6$IJuzB39ajEELQigc$BurZefRiK/Sehk9UQDdVOljc7ccmKQ9iHxcNdU7klUP4ECwWQKKccX7H7ArlJ8Lov.rmwtNg3H3DGBNSnd95A0";
     home = "/home/adwin";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
-  };
-
-  users.users.liugc = {
-    isNormalUser = true;
-    initialHashedPassword = "nohashedpw";
-    home = "/home/liugc";
-    shell = pkgs.bash;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCXwWJH+vsxzntGdP0Cn4piSp1Tocg98YIrvzQIaHbL9wW/1Jj5w9bOYtFP5XbWQSQjbHqH04ISB6boV08Pb41Fs3iCwVXMdUa6qkRh9z0UmXq74Vp58AJ3ONQFOQM/IYkbMFVWE1TjbrXlA/dpPhXKBdCj2ZA7gParqXEfk6KAVNKnFED02YvqoVotOzcfH9nlsMzMRVpfm6he0aP04RZE/Bs/UXzuQXZEwnOBYpuDSLW+CQcoxGhEKgTxgDnfdLNqYyp6rVHWy0+b46fbx1JVU02xMH8YplrIC/b/ysBVboCPf79gZPnw7jQNf+EX9sAm2bNuje1DSSGqivpLe199 adwin@Tardis" 
+    ];
   };
 
   # Enable home manager
@@ -206,7 +142,6 @@
    useGlobalPkgs = true;
    useUserPackages = true;
    users.adwin = import ./home.nix;
-   users.liugc = import ./home-liugc.nix;
   };
    
 
@@ -215,113 +150,11 @@
   environment.systemPackages = with pkgs; [
     wireguard-tools
     frp
-    v2ray
-    qv2ray
     bind
     lsof
   ];
  
-  programs = {
-    sway.enable = true;
-      # sway = {
-        # enable = true;
-        # wrapperFeatures = { base = true; gtk = true; };
-        # extraPackages = with pkgs; [
-          # xwayland
-          # swaylock
-          # swayidle
-          # wl-clipboard
-          # mako # notification daemon
-          # alacritty # Alacritty is the default terminal in the config
-          # wofi # Dmenu is the default in the config but i recommend wofi since its wayland native
-        # ];
-        # extraSessionCommands = ''
-          # export LIBSEAT_BACKEND=logind
-          # export XDG_SESSION_TYPE=wayland
-          # export XDG_SESSION_DESKTOP=sway
-          # export XDG_CURRENT_DESKTOP=sway
-          # export XKB_DEFAULT_LAYOUT=us
-          # export WLR_NO_HARDWARE_CURSORS=1
-          # export SDL_VIDEODRIVER=wayland
-          # export QT_QPA_PLATFORM=wayland
-          # export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-          # export _JAVA_AWT_WM_NONREPARENTING=1
-          # export MOZ_ENABLE_WAYLAND=1
-        # '';
-      # };
-      # neovim = {
-        # enable = true;
-        # package = pkgs.neovim-nightly;
-        # vimAlias = true;
-        # viAlias = true;
-        # defaultEditor = true;
-        # configure = {
-          # customRC = ''
-            # set wildmode=longest,list,full
-            # syntax on
-            # set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
-            # set backspace=indent,eol,start
-            # set number
-            # set showcmd
-            # set ignorecase
-            # set autoindent
-            # set incsearch
-            # set nowrap
-            # set scrolloff=8
-            # set sidescroll=1
-            # set sidescrolloff=8
-            # set cmdwinheight=1
-            # set clipboard+=unnamedplus
 
-            # filetype plugin indent on
-            # let g:tex_flavor = "latex"
-
-
-            # " Custom mappings
-            # let mapleader=";"
-            # inoremap jj <Esc>
-            # nnoremap : q:i
-            # inoremap <C-s> <Esc>:w<CR>a
-            # nnoremap <C-s> :w<CR>
-
-             # " Cycle through completions with tab
-            # inoremap <expr> <Tab>   pumvisible() ? "\<C-n>" : "\<Tab>"
-            # inoremap <expr> <S-Tab> pumvisible() ? "\<C-p>" : "\<S-Tab>"
-            # set completeopt=menuone,noinsert,noselect
-            # set shortmess+=c
-
-            # " Nerd commenter
-            # "
-            # " Use compact syntax for prettified multi-line comments
-            # let g:NERDCompactSexyComs = 1
-            # " Add spaces after comment delimiters by default
-            # let g:NERDSpaceDelims = 1
-            # " Enable trimming of trailing whitespace when uncommenting
-            # let g:NERDTrimTrailingWhitespace = 1
-
-            # '';
-            # packages.vim = {
-            # start = with pkgs.vimPlugins; [
-              # onedark-nvim
-              # vim-airline
-              # vim-airline-themes
-              # nvim-lspconfig
-              # completion-nvim
-              # vim-nix
-              # nerdcommenter
-            # ];
-          # };
-        # };
-      # };
-    };
-
-#   systemd.user.targets.sway-session = {
-#     description = "Sway compositor session";
-#     documentation = [ "man:systemd.special(7)" ];
-#     bindsTo = [ "graphical-session.target" ];
-#     wants = [ "graphical-session-pre.target" ];
-#     after = [ "graphical-session-pre.target" ];
-#   };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -336,49 +169,30 @@
   # Enable the OpenSSH daemon.
   services = {
     fail2ban.enable = true;
-    openssh.enable = true;
+    openssh = {
+      enable = true;
+      passwordAuthentication = false;
+    };
   };
 
   environment.etc = {
-    frp.source = /home/adwin/.config/frp;
-    "v2ray/config.json".text = builtins.readFile ./v2ray.json;
+    # frp.source = /home/adwin/.config/frp;
   };
 
-  systemd.services.frpc = {
-    enable = true;
-    description = "Frp client to expose ssh";
-    unitConfig = {
-      Type = "simple";
-    };
-    serviceConfig = {
-      ExecStart = "${pkgs.frp}/bin/frpc -c /etc/frp/frpc.ini";
-    };
-    wantedBy = [ "multi-user.target" ];
-    after = ["network.target"];
-  };
+  # systemd.services.frpc = {
+    # enable = true;
+    # description = "Frp client to expose ssh";
+    # unitConfig = {
+      # Type = "simple";
+    # };
+    # serviceConfig = {
+      # ExecStart = "${pkgs.frp}/bin/frpc -c /etc/frp/frpc.ini";
+    # };
+    # wantedBy = [ "multi-user.target" ];
+    # after = ["network.target"];
+  # };
 
-  systemd.services.netadapter = {
-    enable = true;
-    description = "rtl8188gu adapter usb mode switch";
-    unitConfig = {
-      Type = "oneshot";
-    };
-    serviceConfig = {
-      ExecStart = "${pkgs.usb-modeswitch}/bin/usb_modeswitch -KW -v 0bda -p 1a2b && ${pkgs.usb-modeswitch}/bin/usb_modeswitch -KW -v 0bda -p 1a2b";
-    };
-    wantedBy = [ "multi-user.target" ];
-    after = ["network.target"];
-  };
 
-  systemd.services.v2ray = {
-    description = "a platform for building proxies to bypass network restrictions";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
-    serviceConfig = {
-      DynamicUser = true;
-      ExecStart = "${pkgs.v2ray}/bin/v2ray -c /etc/v2ray/config.json";
-    };
-  };
 
   systemd.services.NetworkManager-wait-online.enable = false;
 
@@ -389,7 +203,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "unstable"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
 
--- nixos/bluespace/home.nix
+++ nixos/bluespace/home.nix
@@ -1,57 +1,31 @@
 { pkgs, lib, config, ...}:
 {
   home.packages = with pkgs; [
-    black
+    # black
     socat
     htop
     lsof
-    foot
-    wlroots
-    kanshi
-    wofi
-    wl-clipboard
-    clipman
-    # i3status-rust
-    mako
     tree
     ripgrep-all
     ripgrep
-    ht-rust
-    chromium 
+    xh
     file
     trash-cli
     usbutils
     pciutils
-    usb-modeswitch
     nix-prefetch-github
-    nodePackages.pyright
-    rnix-lsp
-    ccls
-    rust-analyzer
-    clang
-    gopls
-    delve
+    # nodePackages.pyright
+    # nil
+    # ccls
+    # gopls
+    # delve
     tealdeer
-    graphviz
     # rust-bin.stable.latest.default
-    poetry
-    v2t
-    fortran-language-server
+    # poetry
+    # fortran-language-server
     exa
     zellij
   ];
-
-  xdg.configFile."nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-c}/parser";
-  xdg.configFile."nvim/parser/cpp.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-cpp}/parser";
-  xdg.configFile."nvim/parser/lua.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-lua}/parser";
-  xdg.configFile."nvim/parser/rust.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-rust}/parser";
-  xdg.configFile."nvim/parser/python.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-python}/parser";
-  xdg.configFile."nvim/parser/nix.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-nix}/parser";
-  xdg.configFile."nvim/parser/json.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-json}/parser";
-  xdg.configFile."nvim/parser/latex.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-latex}/parser";
-  xdg.configFile."nvim/parser/go.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-go}/parser";
-  xdg.configFile."nvim/parser/markdown.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-markdown}/parser";
-  xdg.configFile."nvim/parser/julia.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-julia}/parser";
 
   programs = {
     direnv = {
@@ -97,107 +71,8 @@
         }
       ];
     };
-    alacritty = {
-      enable = true;
-    };
     neovim = import ../../programs/cli/neovim/neovim.nix { 
       inherit pkgs;
     };  
-  };
-
-  systemd.user.sessionVariables = {
-    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
-    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
-  };
-
-
-  wayland.windowManager.sway = {
-    enable = true;
-    systemdIntegration = true;
-    wrapperFeatures = {
-      gtk = true;
-    };
-    extraSessionCommands = ''
-      export WLR_NO_HARDWARE_CURSORS=1
-    '';
-    config = {
-      gaps = {
-        smartBorders = "on";
-      };
-      modifier = "Mod4";
-      menu = "${pkgs.wofi}/bin/wofi";
-      terminal = "${pkgs.alacritty}/bin/alacritty";
-      keybindings =
-        let
-          mod = config.wayland.windowManager.sway.config.modifier;
-          cfg = config.wayland.windowManager.sway.config;
-        in
-        lib.mkOptionDefault {
-          "${mod}+Shift+q" = "exit";
-          "${mod}+t" = "exec ${cfg.terminal}";
-          "${mod}+m" = "exec ${cfg.menu} -S drun";
-          "${mod}+b" = "exec ${pkgs.chromium}/bin/chromium";
-          "${mod}+Return" = "exec ${pkgs.foot}/bin/foot";
-          "${mod}+h" = "splith";
-        };
-        colors = {
-          focused = {
-            background = "#b16286";
-            border = "#b16286";
-            childBorder = "#b16286";
-            indicator = "#b16286";
-            text = "#ebdbb2";
-          };
-          focusedInactive = {
-            background = "#689d6a";
-            border = "#689d6a";
-            childBorder = "#689d6a";
-            indicator = "#689d6a";
-            text = "#ebdbb2";
-          };
-          unfocused = {
-            background = "#3c3836";
-            border = "#3c3836";
-            childBorder = "#3c3836";
-            indicator = "#3c3836";
-            text = "#ebdbb2";
-          };
-          urgent = {
-            background = "#cc241d";
-            border = "#cc241d";
-            childBorder = "#cc241d";
-            indicator = "#cc241d";
-            text = "#ebdbb2";
-          };
-          placeholder = {
-            background = "#000000";
-            border = "#000000";
-            childBorder = "#000000";
-            indicator = "#000000";
-            text = "#ebdbb2 ";
-          };
-        };
-      input = {
-        "type:keyboard" = {
-          repeat_delay = "300";
-          repeat_rate = "20";
-        };
-        "type:touchpad" = {
-          dwt = "enabled";
-          middle_emulation = "enabled";
-          natural_scroll = "enabled";
-          tap = "enabled";
-        };
-      };
-      window = {
-        titlebar = false;
-        hideEdgeBorders = "smart";
-      };
-      startup = [
-        { command = "exec ${pkgs.wl-clipboard}/bin/wl-paste -p -t test --watch clipman store -P -- histpath=\"/tmp/clipman-primary.json\""; }
-        # { command = "${lib.getBin pkgs.dbus}/bin/dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY DBUS_SESSION_BUS_ADDRESS SWAYSOCK XDG_SESSION_TYPE XDG_SESSION_DESKTOP XDG_CURRENT_DESKTOP"; } #workaround
-        # { command = "${pkgs.alacritty}/bin/alacritty"; }
-      ];
-    };
   };
 }
