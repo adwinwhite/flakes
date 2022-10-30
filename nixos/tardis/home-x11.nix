f227c62b514f92305bec87b0f5d2b74cbb8992cb
Adwin White <adwinw01@gmail.com>
2022-10-30 13:10:13 +0800

    update: new bluspace config for new vps

--- nixos/tardis/home-x11.nix
+++ nixos/tardis/home-x11.nix
@@ -172,10 +172,5 @@
     };  
   };
 
-  home.sessionVariables = {
-    GOOGLE_DEFAULT_CLIENT_ID = "77185425430.apps.googleusercontent.com";
-    GOOGLE_DEFAULT_CLIENT_SECRET = "OTJgUOQcT7lO7GsGZq2G4IlT";
-  };
-
   home.stateVersion = "22.05";
 }
