3eb00d7f18011a2e6ec79b5fcdc160007b758cbf
Adwin White <adwinw01@gmail.com>
2023-07-05 22:55:16 +0800

    Add basic flake template

---
+++ templates/basic/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic project devShell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = [
+            pkgs.hello
+          ];
+
+          # shellHook = ''
+            # alias ls=exa
+            # alias find=fd
+          # '';
+        };
+      }
+    );
+}
