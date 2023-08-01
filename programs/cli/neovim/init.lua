e977d424bd67b07a803efb3df602ae91b2fb8d5c
Adwin White <adwinw01@gmail.com>
2023-08-01 21:54:07 +0800

    Fix neovim tab and updated rust template

--- programs/cli/neovim/init.lua
+++ programs/cli/neovim/init.lua
@@ -40,6 +40,20 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
+-- Cancel luasnip session when changing mode.
+-- More at https://github.com/L3MON4D3/LuaSnip/issues/258
+vim.api.nvim_create_autocmd('ModeChanged', {
+  pattern = '*',
+  callback = function()
+    if ((vim.v.event.old_mode == 's' and vim.v.event.new_mode == 'n') or vim.v.event.old_mode == 'i')
+        and require('luasnip').session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not require('luasnip').session.jump_active
+    then
+      require('luasnip').unlink_current()
+    end
+  end
+})
+
 -- Vimtex
 g.vimtex_view_method = "zathura"
 g.vimtex_compiler_latexmk_engines = {
