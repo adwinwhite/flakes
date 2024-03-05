bc13b737bf335c68f1a948dcb269cfd9a20006fa
Adwin White <adwinw01@gmail.com>
2024-03-05 12:12:37 +0800

    fix: fcitx5 in plasma6

--- nixos/tardis/configuration.nix
+++ nixos/tardis/configuration.nix
@@ -47,11 +47,15 @@
     defaultLocale = "en_US.UTF-8";
     inputMethod = {
       enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-chinese-addons
-        fcitx5-pinyin-zhwiki
-        fcitx5-pinyin-moegirl
-      ];
+      fcitx5 = {
+        addons = with pkgs; [
+          fcitx5-chinese-addons
+          fcitx5-pinyin-zhwiki
+          fcitx5-pinyin-moegirl
+        ];
+        waylandFrontend = true;
+        plasma6Support = true;
+      };
     };
   };
 
