92569ac441282d6a09ab6122e2af86953aa7ef68
Adwin White <adwinw01@gmail.com>
2024-10-08 14:32:17 +0800

    fish shell under vi mode shares system clipboard

--- nixos/tardis/home.nix
+++ nixos/tardis/home.nix
@@ -255,6 +255,9 @@ in
       };
       shellInit = ''
         fish_vi_key_bindings
+        bind -M visual y fish_clipboard_copy
+        bind -M normal yy fish_clipboard_copy
+        bind p fish_clipboard_paste
         bind -M insert -m default jj backward-char force-repaint
         bind -M insert \cf forward-char
         bind -M insert \cg forward-word
