e45fa4c5a428db821cd64cf0a7574f6239e8bb32
Adwin White <adwinw01@gmail.com>
2025-03-15 19:10:16 +0800

    revert: use plasma

--- nixos/tardis/home.nix
+++ nixos/tardis/home.nix
@@ -1,4 +1,4 @@
-{ pkgs, osConfig, config, ...}: 
+{ pkgs, osConfig, ...}: 
 let
   chromium = let
     wrapped = pkgs.writeShellScriptBin "chromium" ''
@@ -31,9 +31,6 @@ let
 in
 {
   home.packages = with pkgs; [
-    fuzzel
-    alacritty
-    niri
     delta
     aider-chat
     libnotify
@@ -97,7 +94,6 @@ in
     usbutils
     pciutils
     nix-prefetch-github
-    pyright
     # ccls            # c/c++ lsp
     # clang
     # rust-analyzer # use rustup's one to sync rustc and RA.
@@ -119,7 +115,6 @@ in
   xdg = {
     enable = true;
     configFile = {
-      "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "/home/adwin/flakes/nixos/tardis/niri.kdl";
       # "zellij/config.yaml".text = builtins.readFile ./../../programs/cli/zellij.yaml;
       "wezterm/wezterm.lua".text = builtins.readFile ./wezterm.lua;
       "git/gitignore_global".text = builtins.readFile ./gitignore_global;
@@ -142,7 +137,7 @@ in
           swipe = 0.5;
         };
         swipe = {
-          "4" = {
+          "3" = {
             left = {
               # command = "xdotool key ctrl+shift+Tab";
               command = "ydotool key 29:1 42:1 15:1 15:0 42:0 29:0";
@@ -153,11 +148,11 @@ in
               command = "ydotool key 29:1 15:1 15:0 29:0";
               # command = "dotool key ctrl+Tab";
             };
-            # up = {
-              # # command = "xdotool key Super_L+w";
-              # command = "ydotool key 125:1 17:1 17:1 125:0";
-              # # command = "dotool key super+w";
-            # };
+            up = {
+              # command = "xdotool key Super_L+w";
+              command = "ydotool key 125:1 17:1 17:1 125:0";
+              # command = "dotool key super+w";
+            };
           };
         };
       };
@@ -177,12 +172,6 @@ in
   };
 
   programs = {
-    waybar = {
-      enable = true;
-      settings = [ (import ./waybar.nix { inherit pkgs; }) ];
-      style = builtins.readFile ./waybar.css;
-      systemd.enable = true;
-    };
     zoxide = {
       enable = true;
       options = [ "--cmd" "cd" ];
