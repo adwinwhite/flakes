52b6d4f2ac88f3f45ed5159986ceece352deefdc
Adwin White <adwinw01@gmail.com>
2025-07-07 20:14:10 +0800

    add some binds to niri and wezterm

--- nixos/tardis/wezterm.lua
+++ nixos/tardis/wezterm.lua
@@ -1,59 +1,39 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
+local act = wezterm.action
 
-local mykeys = {}
+local mykeys = {
+	{ key = "t", mods = "CTRL|SHIFT", action = act.SpawnTab "CurrentPaneDomain" },
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CopyTo "Clipboard" },
+	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom "Clipboard" },
+	{ key = "f", mods = "CTRL|SHIFT", action = act.Search { Regex = "", } },
+	{ key = "r", mods = "CTRL|SHIFT", action = act.ReloadConfiguration },
+  { key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
+  { key = "Tab", mods = "CTRL|SHIFT", action = act.ActivateTabRelative(-1) },
+}
+
 for i = 1, 9 do
-  -- CTRL+ALT + number to activate that tab
-  table.insert(mykeys, {
-    key=tostring(i),
-    mods="ALT",
-    action=wezterm.action{ActivateTab=i-1},
-  })
+	-- CTRL+ALT + number to activate that tab
+	table.insert(mykeys, {
+		key = tostring(i),
+		mods = "ALT",
+		action = wezterm.action({ ActivateTab = i - 1 }),
+	})
 end
 
-table.insert(mykeys, {
-  key="t",
-  mods="CTRL|SHIFT",
-  action=wezterm.action{SpawnTab="CurrentPaneDomain"}
-})
-
-table.insert(mykeys, {
-  key="c",
-  mods="CTRL|SHIFT",
-  action=wezterm.action{CopyTo="Clipboard"}
-})
-
-table.insert(mykeys, {
-  key="v",
-  mods="CTRL|SHIFT",
-  action=wezterm.action{PasteFrom="Clipboard"}
-})
-
-table.insert(mykeys, {
-  key="f",
-  mods="CTRL|SHIFT",
-  action=wezterm.action{Search={Regex=""}}
-})
-
-table.insert(mykeys, {
-  key="r",
-  mods="CTRL|SHIFT",
-  action="ReloadConfiguration"
-})
-
 return {
-  check_for_updates = false,
-  enable_wayland = true,
-  enable_scroll_bar = false,
-  color_scheme = "Afterglow",
-  window_padding = {
-    left = 0,
-    right = "1px",
-    top = 0,
-    bottom = 0,
-  },
-  font = wezterm.font("FiraCode Nerd Font"),
-  font_size = 11.0,
-  enable_tab_bar = false,
-  disable_default_key_bindings = true,
-  keys = mykeys
+	check_for_updates = false,
+	enable_wayland = true,
+	enable_scroll_bar = false,
+	color_scheme = "Afterglow",
+	window_padding = {
+		left = 0,
+		right = "1px",
+		top = 0,
+		bottom = 0,
+	},
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 11.0,
+	enable_tab_bar = false,
+	disable_default_key_bindings = true,
+	keys = mykeys,
 }
